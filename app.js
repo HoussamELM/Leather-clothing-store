fe50481dca69b77a19c973ab75f3c782b74313f7
houssamelm <houssamelmasoudi@gmail.com>
2022-10-30 08:32:41 +0100

    leave animations

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-const tlLeave = gsap.timeline({defaults: {duration: 0.75, ease: "Power2.easeOut"}})
-const tlEnter = gsap.timeline({defaults: {duration: 0.75, ease: "Power2.easeOut"}})
+const tlLeave = gsap.timeline({ defaults: { duration: 0.75, ease: "Power2.easeOut" } })
+const tlEnter = gsap.timeline({ defaults: { duration: 0.75, ease: "Power2.easeOut" } })
 
+const leaveAnimation = (current, done) => {
+    const product = current.querySelector('.image-container')
+    const text = current.querySelector(".showcase-text")
+    const circles = current.querySelectorAll(".circle")
+    const arrow = current.querySelector(".showcase-arrow")
 
+    return (
+        tlLeave.fromTo(
+            arrow,
+            { opacity: 1, y: 0 },
+            { opacity: 0, y: 50 }
+    ),
+        tlLeave.fromTo(product, {y: 0, opacity: 1}, {y: 100, opacity: 0, onComplete: done}, "<"
+        ),
+        tlLeave.fromTo(text, {y: 0, opacity: 1}, {y: 100, opacity: 0, onComplete: done}, "<"
+        ),
+        tlLeave.fromTo(circles, {y: 0, opacity: 1}, {y: -200, opacity: 0, stagger: 0.05}, "<"
+        )
+    
+    )}
 
 barba.init({
     preventRunning: true,
     transitions: [
         {
             name: 'default',
-            leave(data){
+            leave(data) {
                 const done = this.async()
                 let current = data.current.container;
-                gsap.fromTo(
-                   current,
-                     {opacity: 1},
-                     {opacity: 0, duration: 0.6, onComplete: done},
-                );
+                leaveAnimation(current, done)
             },
-            enter(data){
+            enter(data) {
                 const done = this.async()
                 let next = data.next.container;
                 gsap.fromTo(
                     next,
-                      {opacity: 0},
-                      {opacity: 1, duration: 0.6, onComplete: done},
-                 );
+                    { opacity: 0 },
+                    { opacity: 1, duration: 1, onComplete: done },
+                );
             }
         }
     ]
