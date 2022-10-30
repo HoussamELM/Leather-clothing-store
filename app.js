3ffd22831237635e79960a0f3ce44992f3a4a528
houssamelm <houssamelmasoudi@gmail.com>
2022-10-30 09:35:55 +0100

    smoothed things out and fixed refresh issue

--- app.js
+++ app.js
@@ -12,37 +12,41 @@ const leaveAnimation = (current, done) => {
             arrow,
             { opacity: 1, y: 0 },
             { opacity: 0, y: 50 }
-    ),
-        tlLeave.fromTo(product, {y: 0, opacity: 1}, {y: 100, opacity: 0, onComplete: done}, "<"
         ),
-        tlLeave.fromTo(text, {y: 0, opacity: 1}, {y: 100, opacity: 0, onComplete: done}, "<"
+        tlLeave.fromTo(product, { y: 0, opacity: 1 }, { y: 100, opacity: 0, onComplete: done }, "<"
         ),
-        tlLeave.fromTo(circles, {y: 0, opacity: 1}, {y: -200, opacity: 0, stagger: 0.05}, "<"
+        tlLeave.fromTo(text, { y: 0, opacity: 1 }, { y: 100, opacity: 0, onComplete: done }, "<"
+        ),
+        tlLeave.fromTo(circles, { y: 0, opacity: 1 }, { y: -200, opacity: 0, stagger: 0.15, ease: "back.out(3)", duration: 1 }, "<"
         )
-    
-    )}
 
-    
-        const enterAnimation = (current, done, gradient) => {
-        const product = current.querySelector('.image-container')
-        const text = current.querySelector(".showcase-text")
-        const circles = current.querySelectorAll(".circle")
-        const arrow = current.querySelector(".showcase-arrow")
-    
-        return (
-            tlEnter.fromTo(arrow,   { opacity: 0, y: 50 },{ opacity: 1, y: 0 }
-            ),
-            tlEnter.To('body', {background: gradient}, "<"
-            ),
-            tlEnter.fromTo(product, {y: -100, opacity: 0}, {y: 0, opacity: 1, onComplete: done}, "<"
-            ),
-            tlEnter.fromTo(text,    {y: 100, opacity: 0}, {y: 0, opacity: 1, onComplete: done}, "<"
-            ),
-            tlEnter.fromTo(circles, {y: -200, opacity: 0}, {y: 0, opacity: 1, stagger: 0.05}, "<"
-            )
-            
-        
-        )}
+    )
+}
+
+
+const enterAnimation = (current, done, gradient) => {
+    console.log(gradient)
+    const product = current.querySelector('.image-container')
+    const text = current.querySelector(".showcase-text")
+    const circles = current.querySelectorAll(".circle")
+    const arrow = current.querySelector(".showcase-arrow")
+    const body = current.querySelector("body")
+
+    return (
+        tlEnter.fromTo(arrow, { opacity: 0.5, y: 50 }, { opacity: 1, y: 0 }
+        ),
+        tlEnter.to("body", {background: gradient}, "<"
+        ),
+        tlEnter.fromTo(product, { y: -100, opacity: 0 }, { y: 0, opacity: 1, onComplete: done }, "<"
+        ),
+        tlEnter.fromTo(text, { y: 100, opacity: 0 }, { y: 0, opacity: 1, onComplete: done }, "<"
+        ),
+        tlEnter.fromTo(circles, { y: -200, opacity: 0 }, { y: 0, opacity: 1, stagger: 0.15, ease: "back.out(3)", duration: 1 }, "<10%"
+        )
+
+
+    )
+}
 
 
 barba.init({
@@ -50,6 +54,13 @@ barba.init({
     transitions: [
         {
             name: 'default',
+            once(data){
+                const done = this.async
+                let next = data.next.container
+                let gradient = getGradient(data.next.namespace)
+                gsap.set("body", {background: gradient});
+                enterAnimation(next, done, gradient)
+            },
             leave(data) {
                 const done = this.async()
                 let current = data.current.container;
@@ -65,15 +76,16 @@ barba.init({
     ]
 })
 
-function getGradient(name)
-{
-    switch(name){
+function getGradient(namespace) {
+    switch (namespace) {
         case "handbag":
             return "linear-gradient(260deg, #b75d62, #754d4f)";
+            break;
         case "boot":
             return "linear-gradient(260deg, #5d8cb7, #4c4f70)";
+            break;
         case "hat":
-            return "linear-gradient(260deg, #b27a5c, #754d4f)";
+            return "linear-gradient(260deg, #b27a5c, #a7f5450)";
+            break;
     }
-    console.log("changed to", name);
 }
